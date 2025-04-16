da79d7fcc898b4d2abc5679d0d74eaa5667a06d9
Emre Karaaslan <emre@192.168.1.103>
2025-04-16 16:18:01 +0300

    added some files

---
+++ terraform/backend/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket         = "terraform-state-codeway"
+  force_destroy  = true
+
+  tags = {
+    Name        = "Terraform State"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Locks"
+    Environment = "dev"
+  }
+}
