e9b672b6643efd098253f3b67b247d0ec970b3ea
Emre Karaaslan <emre@192.168.1.103>
2025-04-18 20:06:03 +0300

    dummy test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
 
 require("./app/routes/tutorial.routes.js")(app);
 
-console.log(`Server is running`);
+console.log(`Server is running. just for test`);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
