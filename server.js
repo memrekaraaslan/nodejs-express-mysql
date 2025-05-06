3f79d64049157e71e2c4079e912af8342ab21e4f
Emre Karaaslan <emre@192.168.1.103>
2025-05-06 13:24:42 +0300

    dummy test for build process

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/tutorial.routes.js")(app);
-
+// test
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
